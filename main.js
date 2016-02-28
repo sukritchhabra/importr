47aa998e7cc1271634422e1b8e6a522e5f281849
Sukrit Chhabra <sukrit.chhabra@gmail.com>
2016-02-28 05:16:22 -0500

    Working with Bootstrap as a template to create functionality

--- main.js
+++ main.js
@@ -3,8 +3,28 @@
 /*global $, jQuery, alert*/
 
 $(document).ready(function() {
+    function getLibraryInfo(libraryName) {
+        var info;
+        $.ajax({
+            url: "/libraries/" + libraryName + "/package.json",
+            type: "GET",
+            async: false,
+            success: function (response) {
+                info = JSON.parse(response);
+                console.log('recieved json:');
+                console.log(info);
+            },
+            error: function (errorReport) {
+                console.log('Error happened in AJAX Request!!');
+            }
+        });
+        return info;
+    }
+
     $('body').on('search', function(event) {
-        var searchedString = event.searchString;
-        console.log('searched: '+searchedString);
+        var searchedLibrary = event.searchString;
+        console.log('searched: ' + searchedLibrary);
+
+        var libraryInfo = getLibraryInfo(searchedLibrary);
     });
 })
