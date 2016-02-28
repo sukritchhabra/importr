d78b58c4d13ccaeeaf52048ff47697841b4bf11c
Sukrit Chhabra <sukrit.chhabra@gmail.com>
2016-02-28 05:34:14 -0500

    Getting ready from Github/RawGit

--- main.js
+++ main.js
@@ -3,6 +3,11 @@
 /*global $, jQuery, alert*/
 
 $(document).ready(function() {
+    /**
+     * A function to get a libraries JSON file via AJAX
+     * @param  {[String]} libraryName   [Name of the library, the information of which is needed]
+     * @return {[JSON]}                 [JSON object/file acuired via AJAX]
+     */
     function getLibraryInfo(libraryName) {
         var info;
         $.ajax({
@@ -21,10 +26,30 @@ $(document).ready(function() {
         return info;
     }
 
+    function getReadme(readmeLink) {
+        var readme;
+        $.ajax({
+            url: readmeLink,
+            type: "GET",
+            async: false,
+            success: function (response) {
+                readme = response;
+                console.log('recieved readme:');
+                console.log(readme);
+            },
+            error: function (errorReport) {
+                console.log('Error happened in AJAX Request!!');
+            }
+        });
+        return readme;
+    }
+
     $('body').on('search', function(event) {
         var searchedLibrary = event.searchString;
         console.log('searched: ' + searchedLibrary);
 
         var libraryInfo = getLibraryInfo(searchedLibrary);
+        var libReadme = getReadme(libraryInfo.readme);
+
     });
 })
